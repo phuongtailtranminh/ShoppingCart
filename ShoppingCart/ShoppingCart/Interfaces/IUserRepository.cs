e952e73fbf0c11c286bdaec4f8bf2b9ba82021d9
Phuong Tran <Phuong Tran>
2016-10-29 19:39:20 +0700

    Added Repository

---
+++ ShoppingCart/ShoppingCart/Interfaces/IUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Interfaces
+{
+    interface IUserRepository : IDisposable
+    {
+        IEnumerable<user> GetUsers();
+        user GetUserById(int userId);
+        void InsertUser(user user);
+        void DeleteUserById(int userId);
+        void UpdateUser(user user);
+        void Save();
+    }
+}
