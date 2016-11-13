ac1577bcb5ccdf0ae5b77baffaf8cec150b894e2
Phuong Tran <Phuong Tran>
2016-11-13 16:20:46 +0700

    Added Product Detail Controller

---
+++ ShoppingCart/ShoppingCart/Controllers/ProductXController.cs
@@ -0,0 +1,26 @@
+﻿using ShoppingCart.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingCart.Controllers
+{
+    public class ProductXController : Controller
+    {
+        private IProductRepository ProductRepository;
+        private ShoppingCartDb shoppingCart = new ShoppingCartDb();
+        public ProductXController()
+        {
+            this.ProductRepository = new ProductRepository(shoppingCart);
+        }
+
+        // GET: Product
+        public ActionResult Index(int id)
+        {
+            var product = ProductRepository.GetProductById(id);
+            return View(product);
+        }
+    }
+}
